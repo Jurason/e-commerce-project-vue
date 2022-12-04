bb29ff5c00a7d547f63bd745fb9c8200e42bfc0b
Eugene <evgenilymanyuk@gmail.com>
2022-12-05 00:01:36 +0200

    map component add/remove markers

--- src/MapExperiments/MapView.vue
+++ src/MapExperiments/MapView.vue
@@ -1,18 +1,27 @@
 <template>
 	<div class="map-comp-container">
+		<BaseButton
+				:disabled="isAlreadyAdded || false"
+				name="Add marker"
+				@click="addMarkerOnMap"
+		/>
 		<div class="search-container">
-			<MapSearchInput @searchQuery="searchQueryHandler($event)"/>
+			<MapSearchInput
+					@searchQuery="searchQueryHandler($event)"
+			/>
 		</div>
 		<div class="map-container">
 			<MapComponent
-			:coordinatesToDisplay="coordinatesToDisplay"
-			:markersList="dataToDisplay"
+			:coordinatesToDisplay="coordsToDisplay"
 			:markerToRemove="markerToRemove"
+			:markerToAdd="markerToAdd"
+			@add-to-table="addRowToTable($event)"
 			/>
 			<div class="marker-table">
 				<MapDataTable
-				:itemsList="dataToDisplay"
-				@remove-item="removeItem($event)"
+				:itemsList="tableData"
+				@remove-item="removeRow($event)"
+				@show-item="this.dataToDisplay = $event"
 				/>
 			</div>
 		</div>
@@ -22,61 +31,74 @@
 import MapComponent from "./components/MapComponent";
 import MapSearchInput from "./components/MapSearchInput";
 import MapDataTable from "./components/MapDataTable";
+import BaseButton from "../components/BaseButton";
 import { getCoords } from "./api";
-export const INITIAL_COORDS = {
-	latitude: 46.44,
-	longitude: 30.71,
-	zoom: 6
-}
+
 export default {
 	name: "MapView",
 	components: {
 		MapDataTable,
 		MapComponent,
-		MapSearchInput
-	},
-	mounted(){
-		if(localStorage.getItem('marker-list')){
-			this.dataToDisplay = JSON.parse(localStorage.getItem('marker-list'))
-		}
+		MapSearchInput,
+		BaseButton
 	},
 	data(){
 		return {
-			dataToDisplay: [],
-			coordinatesToDisplay: {
-				latitude: INITIAL_COORDS.latitude,
-				longitude: INITIAL_COORDS.longitude,
-				zoom: INITIAL_COORDS.zoom
-			},
-			markerToRemove: null
+			tableData: [],
+			dataToDisplay: null,
+			markerToRemove: null,
+			markerToAdd: null,
 		}
 	},
+	computed: {
+		isAlreadyAdded(){
+			if(!this.dataToDisplay){
+				return
+			}
+			return this.tableData.some(row => row['name'] === this.dataToDisplay['name']) || false
+		},
+		coordsToDisplay(){
+			if(!this.dataToDisplay){
+				return
+			}
+			const { latitude, longitude, zoom } = this.dataToDisplay
+			return { latitude: latitude, longitude: longitude, zoom: zoom }
+		},
+	},
 	methods: {
-		removeItem(item){
-			this.markerToRemove = item
-			this.dataToDisplay = this.dataToDisplay.filter(item => item['name'] !== item['name'])
+		addMarkerOnMap(){
+			this.markerToAdd = this.dataToDisplay
+		},
+		addRowToTable(row){
+			this.tableData.push(row)
+		},
+		removeRow(rowData){
+			this.markerToRemove = rowData
+			this.tableData = this.tableData.filter(el => el['name'] !== rowData['name'])
 		},
 		async searchQueryHandler(query){
 			const responseData = await getCoords(query)
 			if(responseData){
-				this.coordinatesToDisplay = responseData
-				// already added to table ?
-				if(this.dataToDisplay.some(item => item['name'].toLowerCase() === responseData['name'])){
-					return
-				}
-				this.dataToDisplay.push(responseData)
+				this.dataToDisplay = responseData
 			}
 		},
 	},
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 .map-comp-container {
 	display: flex;
 	flex-direction: column;
 	height: 90vh;
+	button {
+		position: absolute;
+		width: 150px;
+		height: 50px;
+		top: 129px;
+		right: 90px;
+	}
 }
 .search-container {
 	margin: 30px auto;
--- src/MapExperiments/components/MapComponent.vue
+++ src/MapExperiments/components/MapComponent.vue
@@ -1,5 +1,7 @@
 //	what this component do?
 // - display the map view at the specified coordinates
+// - add markers
+// - remove markers
 
 <template>
 	<div id="map"></div>
@@ -7,53 +9,80 @@
 
 <script>
 import leaflet from "leaflet";
+export const INITIAL_COORDS = {
+	latitude: 46.44,
+	longitude: 30.71,
+	zoom: 6
+}
 
 export default {
 	name: "MapComponent",
 	props: {
-		coordinatesToDisplay: {type: Object, required: true},
-		markersList: {type: Object, required: false},
+		coordinatesToDisplay: {type: Object, required: false},
 		markerToRemove: {type: Object, required: false},
+		markerToAdd: {type: Object, required: false},
 	},
-	map: null,
+	emits: {
+		'add-to-table': data => typeof data === 'object'
+	},
+	data(){
+		return {
+			map: null,
+			markersGroup: null,
+			initialView: {
+				latitude: INITIAL_COORDS.latitude,
+				longitude: INITIAL_COORDS.longitude,
+				zoom: INITIAL_COORDS.zoom
+			},
+		}
+	},
 	mounted(){
-		const { latitude:lat, longitude:lng, zoom } = this.coordinatesToDisplay
-
-		this.$options.map = leaflet.map('map').setView([lat, lng], zoom);
+		const { latitude:lat, longitude:lng, zoom } = this.initialView
+		this.map = leaflet.map('map').setView([lat, lng], zoom);
 		leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			maxZoom: 19,
 			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-		}).addTo(this.$options.map);
+		}).addTo(this.map);
+		this.markersGroup = leaflet.layerGroup().addTo(this.map)
 
-		this.markersList?.forEach(this.addMarker)
+		if(localStorage.getItem('marker-list')){
+			const markersListFromStorage = JSON.parse(localStorage.getItem('marker-list'))
+			markersListFromStorage.forEach(this.addMarker)
+		}
 	},
 	methods: {
-		setView(){
-			const { latitude:lat, longitude:lng, zoom } = this.coordinatesToDisplay
-			this.$options.map.setView([lat, lng], zoom);
+		setView(coordinates){
+			const { latitude:lat, longitude:lng, zoom } = coordinates
+			this.map.setView([lat, lng], zoom);
 		},
 		addMarker(marker){
-			leaflet.marker([marker.latitude, marker.longitude]).addTo(this.$options.map)
+			const layer = leaflet.marker([marker.latitude, marker.longitude])
+			this.markersGroup.addLayer(layer)
+			this.handleMarkerData(marker, layer)
+		},
+		handleMarkerData(marker, layer){
+			Object.assign(marker, {id: layer._leaflet_id})
+			this.$emit('add-to-table', marker)
 		},
 		removeMarker(marker){
-			console.log('remove marker')
-			this.$options.map.removeLayer(marker)
+			if(!this.markersGroup.hasLayer(marker.id)){
+				return
+			}
+			this.markersGroup.removeLayer(marker.id)
 		}
 	},
 	watch: {
 		coordinatesToDisplay(newValue){
-			this.setView()
-			this.addMarker(newValue)
-			// console.log('this.markersList:', this.markersList)
-
+			this.setView(newValue)
 		},
 		markerToRemove(newValue){
-			this.removeMarker([newValue.latitude, newValue.longitude])
-			console.log('newValue:', newValue)
+			this.removeMarker(newValue)
+		},
+		markerToAdd(newValue){
+			this.addMarker(newValue)
 		}
 	}
 }
-
 </script>
 
 <style scoped>
